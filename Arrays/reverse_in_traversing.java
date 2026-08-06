ebac5eb5cec39ebd31dbf2dba0ed7b197f1d969b
Tushar Patil <tushardp1800@gmail.com>
2026-08-06 22:57:15 +0530

    Reverse thr element of array

---
+++ Arrays/reverse_in_traversing.java
@@ -0,0 +1,11 @@
+public class reverse_in_traversing {
+    public static void main(String[] args){
+    int[] arr = {10,20,30,40,50};
+
+    for(int i=arr.length-1;i>=0;i--){
+        System.out.println(arr[i]);
+    }
+    
+    
+}
+}
