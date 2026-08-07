96528d4deb98a77bff3e4399296b4cf4c6f02958
Tushar Patil <tushardp1800@gmail.com>
2026-08-08 00:16:27 +0530

    Finding the second maximum element in Array

---
+++ Arrays/SecondmaxArray.java
@@ -0,0 +1,18 @@
+public class SecondmaxArray {
+
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50};
+        int max = Integer.MIN_VALUE;
+        int smax = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] > max) max = arr[i];
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] > smax && arr[i] != max) 
+                smax = arr[i];
+        }
+        System.out.println("Maximum Element is:"+max);
+        System.out.println("Second Maximum Element is:"+smax);
+
+    }
+}
