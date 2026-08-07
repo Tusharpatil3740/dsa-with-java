10ecd8c4a0669bdf8bc53c90d766683d7b39f9d8
Tushar Patil <tushardp1800@gmail.com>
2026-08-07 15:39:55 +0530

    Binery search by user input

---
+++ Arrays/Binary_Search_userinput.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Binary_Search_userinput {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the how many element enter i array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+
+        for(int i=0;i<n;i++){
+
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter the Element to Search");
+        int target = sc.nextInt();
+
+        int low = 0;
+        int high = n-1;
+        boolean found = false;
+
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(arr[mid] == target){
+                System.out.println("Element found at index " + mid);
+                found = true;
+                break;
+                
+
+            }
+            else if(arr[mid] < target) {
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        if(!found){
+            System.out.println("Element is not found");
+        }
+     
+
+    }
+    
+
+    
+    
+}
