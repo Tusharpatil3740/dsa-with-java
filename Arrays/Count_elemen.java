1532a85e4dc2b678179f6d7f5b93bbba2c67acfa
Tushar Patil <tushardp1800@gmail.com>
2026-08-08 12:27:12 +0530

    counting the targeted Array Element

---
+++ Arrays/Count_elemen.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Count_elemen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the number of Element");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        
+        int count = 0;
+        System.out.println("Enter the Elements");
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter the Target value");
+        int target = sc.nextInt();
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] == target) count++;
+
+            else System.out.println("your target Element is not in Array"); break;
+
+        }
+        System.out.println("your Target Element is "+ count + " times in Array");
+
+    
+
+    }
+
+    
+}
