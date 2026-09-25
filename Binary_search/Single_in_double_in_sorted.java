3542d99b818a92100f6ee71412d5eedf7f479def
Tushar Patil <tushardp1800@gmail.com>
2026-09-25 11:56:18 +0530

    finding the single element in array if other element occurs twice if array is sorted

---
+++ Binary_search/Single_in_double_in_sorted.java
@@ -0,0 +1,30 @@
+public class Single_in_double_in_sorted{
+
+    static int single(int[] arr) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (mid % 2 == 1) {
+                mid--;
+            }
+
+            if (arr[mid] == arr[mid + 1]) {
+                low = mid + 2;
+            } else {
+                high = mid;
+            }
+        }
+
+        return arr[low];
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 1, 2, 2, 3, 3, 4, 50, 50, 65, 65};
+
+        System.out.println("Single element = " + single(arr));
+    }
+}
